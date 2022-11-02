32a4a0a5489fd58bfcbdf70794a0b490f8fbfbc1
David Foard <dave.foard@gmail.com>
2022-11-02 18:19:30 +0000

    to and from CSV

---
+++ csvfiles03.go
@@ -0,0 +1,38 @@
+/* RZFeeser | Alta3 Research
+   Reading CSV file - ReadAll() - With custom delimiters*/
+
+
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "log"
+    "os"
+)
+
+func main() {
+
+    log.SetOutput(os.Stderr)
+    f, err := os.Open("origin1.csv")
+
+    if err != nil {
+
+        log.Fatal(err)
+
+    }
+
+    // set the separator and the comment character
+    // so that the package knows how to parse the file
+    r := csv.NewReader(f)
+    r.Comma = ';'            // delimiter
+    r.Comment = '#'          // comment at the top of the file
+
+    records, err := r.ReadAll()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Print(records)
+}    
